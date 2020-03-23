f984238655e51d1238c23014f54eab3547eed430
Lucas Nunes <lucasbanunes@gmail.com>
2020-03-22 22:37:18 -0300

    Added frame_from_history function. Added comments.

--- data_analysis/functions/utils.py
+++ data_analysis/functions/utils.py
@@ -1,2 +1,17 @@
+import pandas as pd
+
 def class_name(obj):
+    """Returns the name of the obj's class"""
     return obj.__class__.__name__
+
+def frame_from_history(self, dict_):
+    """Builds a pandas.DataFrame from the keras.callbacks.callbacks.History.history
+    attribute"""
+    
+    frame = pd.DataFrame.from_dict(dict_)
+    for key in dict_.values():
+        epochs = len(key)
+        break
+    frame.index = pd.Index(range(epochs), name='epoch')
+
+    return frame
