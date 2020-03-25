ac881c7dac27a3cbf65e6c9c9633d56fd9da9eec
Lucas Nunes <lucasbanunes@gmail.com>
2020-03-24 23:40:15 -0300

    Added leave one run out cross validation method for lofar runs.

--- data_analysis/utils/splitters.py
+++ data_analysis/utils/splitters.py
@@ -1,4 +1,5 @@
 import math
+from copy import copy
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -49,3 +50,37 @@ def train_val_split(x_set, y_set, val_percentage=0.0, shuffle=True):
     y_set_val = y_set[split:]
 
     return x_set_train, y_set_train, x_set_val, y_set_val
+
+def leave_one_run_out(classes_runs):
+    """Generator that returns the range of the run left out for test and the rest for fitting
+    
+    Parameters:
+
+    classes_runs: 2-d arraylike
+        Array with first dimension being the number of classes
+        each class has a 2-d tuple. The first element of the tuple is
+        a array with the ranges of the runs and the second element the class.
+
+        Example:
+        class_1_runs = [range(0,100), range(100,200)]
+        class_3_runs =  [range(630, 730)]
+        classes_runs = [class_1_runs, class_3_runs]
+        classes = [1,3]
+        classes_runs = list(zip(classes_runs, classes))
+
+    Yields:
+
+    test_run: numpy.ndarray with shape (1,1)
+        Array with the test run
+    
+    fit_classes_runs: list of lists
+        List with the rest of the runs separated by class
+    """
+    
+    for class_ in range(len(classes_runs)):
+        for run in range(len(classes_runs[class_])):
+            test_run = np.array(classes_runs[class_][run]).reshape(1,1)
+            train_classes_runs = copy(classes_runs)
+            train_classes_runs[class_].pop(run)
+
+            yield test_run, train_classes_runs
