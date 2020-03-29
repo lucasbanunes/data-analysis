eb6575f41e4d792bb3da8261750c14670c4ac67c
Lucas Nunes <lucasbanunes@gmail.com>
2020-03-29 17:11:22 -0300

    Fixed issues with LofarSplitter.

--- data_analysis/model_evaluation/splitters.py
+++ data_analysis/model_evaluation/splitters.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras.utils import Sequence, to_categorical
 from sklearn.model_selection import KFold
+import data_analysis.utils.utils as utils
 from data_analysis.utils.lofar_operators import LofarImgSequence, window_runs
 
 def train_val_split(x_set, y_set, val_percentage=0.0, shuffle=True):
@@ -53,49 +54,14 @@ def train_val_split(x_set, y_set, val_percentage=0.0, shuffle=True):
 
     return x_set_train, y_set_train, x_set_val, y_set_val
 
-def leave_one_run_out(classes_runs):
-    """Generator that returns the range of the run left out for test and the rest for fitting
-    
-    Parameters:
-
-    classes_runs: 2-d arraylike
-        Array with first dimension being the number of classes
-        each class has a 2-d tuple. The first element of the tuple is
-        a array with the ranges of the runs and the second element the class.
-
-        Example:
-        class_1_runs = [range(0,100), range(100,200)]
-        class_3_runs =  [range(630, 730)]
-        classes_runs = [class_1_runs, class_3_runs]
-        classes = [1,3]
-        classes_runs = list(zip(classes_runs, classes))
-
-    Yields:
-
-    test_run: numpy.ndarray with shape (1,1)
-        Array with the test run
-    
-    fit_classes_runs: list of lists
-        List with the rest of the runs separated by class
-    """
-    
-    for class_out in range(len(classes_runs)):
-        for run_out in range(len(classes_runs[class_out])):
-            test_run = np.array(classes_runs[class_out][run_out]).reshape(-1,1)
-            train_classes_runs = copy(classes_runs)
-            train_classes_runs[class_out].pop(run_out)
-
-            yield class_out, run_out, test_run, train_classes_runs
-
-class LofarImgSplitter():
+class LofarSplitter():
 
     def __init__(self, lofar_data, classes_runs, classes, window_size, stride):
         self.lofar_data = lofar_data
         self.classes_runs = classes_runs
         self.window_size = window_size
         self.stride = stride
-        self.nov_cls == None
-        self.val_percentage = 0.0
+        self.nov_cls = None
         self.classes = classes
 
     def set_novelty(self, nov_cls):
@@ -104,19 +70,19 @@ class LofarImgSplitter():
         else:
             raise ValueError('The given novelty class is not on the classes parameter.')
 
-    def compile(self, val_percentage, test_batch, train_batch, val_batch=None, one_hot_encode=False, convolutional_input=True):
+    def compile(self, test_batch, train_batch, val_batch=None, val_percentage=0.0, one_hot_encode=False, convolutional_input=True):
         self.test_batch = test_batch
         self.train_batch = train_batch
         self.val_batch = val_batch
         self.one_hot_encode = one_hot_encode
-        self.convolutional_input = convolutional_input]
+        self.convolutional_input = convolutional_input
         self.val_percentage = float(val_percentage)
 
-    def kfold_split(self, n_splits, test_seq = True, shuffle=False, random_state=None):
+    def kfold_split(self, n_splits, shuffle=False, random_state=None):
         if not self._compiled():
-            raise NameError('The output parameters must be defined before. define them by using the compile method.')
+            raise NameError('The output parameters must be defined before calling this method. define them by using the compile method.')
 
-        x_set, y_set = window_runs(list(zip(self.classes_runs, self.classes)), window_size, stride)
+        x_set, y_set = window_runs(list(zip(self.classes_runs, self.classes)), self.window_size, self.stride)
 
         if not self.nov_cls is None:
             x_known = x_set[y_set != self.nov_cls]
@@ -134,12 +100,17 @@ class LofarImgSplitter():
             if not self.nov_cls is None:
                 x_test = np.concatenate((x_novelty, x_known[test_index]), axis=0)
                 y_test = np.concatenate((y_novelty, y_known[test_index]), axis=0)
+                x_fit = x_known[fit_index]
+                #Compensating from the class taken out as novelty
                 y_fit = np.where(y_known[fit_index]>self.nov_cls, y_known[fit_index]-1, y_known[fit_index])
+                num_classes = len(self.classes)-1
             else:
                 x_test = x_known[test_index]
                 y_test = y_known[test_index]
+                x_fit = x_known[fit_index]
+                #No need to compensate
                 y_fit = y_known[fit_index]
-            x_fit = x_known[fit_index]
+                num_classes = len(self.classes)
 
             val_split = math.ceil(len(x_fit)*self.val_percentage)
             x_val = x_fit[:val_split]
@@ -148,40 +119,39 @@ class LofarImgSplitter():
             y_train = y_fit[val_split:]
             
             train_set = LofarImgSequence(self.lofar_data, x_train, y_train, self.train_batch, self.one_hot_encode, 
-                                         num_classes=len(self.classes), convolutional_input=self.convolutional_input)
+                                         num_classes=num_classes, convolutional_input=self.convolutional_input)
 
-            if test_seq:
-                test_set = LofarImgSequence(self.lofar_data, x_test, y_test, self.test_batch, self.one_hot_encode, 
-                            num_classes=len(self.classes), convolutional_input=self.convolutional_input)
-            else:
-                test_set = (x_test, y_test)
+            x_test_seq = LofarImgSequence(self.lofar_data, x_test, batch_size=self.test_batch, one_hot_encode=self.one_hot_encode, 
+                                          num_classes=num_classes, convolutional_input=self.convolutional_input)
 
-
-            if self.val_percentage == 0.0:
+            if math.isclose(self.val_percentage, 0.0):
                 val_set = LofarImgSequence(self.lofar_data, x_val, y_val, self.val_batch, self.one_hot_encode, 
-                                           num_classes=len(self.classes), convolutional_input=self.convolutional_input)
+                                           num_classes=num_classes, convolutional_input=self.convolutional_input)
                 
-                yield test_set, val_set, train_set
+                yield x_test_seq, y_test, val_set, train_set
             else:
-                yield test_set, train_set
+                yield x_test_seq, y_test, train_set
 
     def leave1run_out_split(self, test_seq = True):
         if not self._compiled():
-            raise NameError('The output parameters must be defined before. define them by using the compile method.')
+            raise NameError('The output parameters must be defined before calling this method. define them by using the compile method.')
 
-        for class_out, run_out, test_run, train_classes_runs in leave_one_run_out(self.classes_runs):
+        for class_out, run_out, test_run, train_classes_runs in self.leave1run_out(self.classes_runs):
             x_set, y_set = window_runs(list(zip(self.classes_runs, self.classes)), self.window_size, self.stride)
-            x_test, y_test = window_runs(list(zip(test_run, np.array(class_out))), self.window_size, self.stride)
+            x_test, y_test = window_runs([(test_run, class_out)], self.window_size, self.stride)
 
             if not self.nov_cls is None:
                 x_fit = x_set[y_set != self.nov_cls]
-                #Compensating for class out for novelty
+                #Compensating from the class taken out as novelty
                 y_fit = np.where(y_set[y_set != self.nov_cls]>self.nov_cls, y_set[y_set != self.nov_cls] - 1, y_set[y_set != self.nov_cls])
                 x_novelty = x_set[y_set == self.nov_cls]
                 y_novelty = y_set[y_set == self.nov_cls]
+                num_classes = len(self.classes)-1
             else:
+                #No need to compensate
                 x_fit = x_set
                 y_fit = y_set
+                num_classes = len(self.classes)
 
             val_split = math.ceil(len(x_fit)*self.val_percentage)
             x_val = x_fit[:val_split]
@@ -190,23 +160,45 @@ class LofarImgSplitter():
             y_train = y_fit[val_split:]
 
             train_set = LofarImgSequence(self.lofar_data, x_train, y_train, self.train_batch, self.one_hot_encode, 
-                                         num_classes=len(self.classes), convolutional_input=self.convolutional_input)
+                                         num_classes=num_classes, convolutional_input=self.convolutional_input)
 
-            if test_seq:
-                test_set = LofarImgSequence(self.lofar_data, x_test, y_test, self.test_batch, self.one_hot_encode, 
-                            num_classes=len(self.classes), convolutional_input=self.convolutional_input)
-            else:
-                test_set = (x_test, y_test)
+            x_test_seq = LofarImgSequence(self.lofar_data, x_test, batch_size=self.test_batch, one_hot_encode=self.one_hot_encode, 
+                                          num_classes=num_classes, convolutional_input=self.convolutional_input)
 
-
-            if self.val_percentage == 0.0:
+            if math.isclose(self.val_percentage, 0.0):
                 val_set = LofarImgSequence(self.lofar_data, x_val, y_val, self.val_batch, self.one_hot_encode, 
-                                           num_classes=len(self.classes), convolutional_input=self.convolutional_input)
+                                           num_classes=num_classes, convolutional_input=self.convolutional_input)
                 
-                yield class_out, run_out, test_set, val_set, train_set
+                yield class_out, run_out, x_test_seq, y_test, val_set, train_set
             else:
-                yield class_out, run_out, test_set, train_set
+                yield class_out, run_out, x_test_seq, y_test, train_set
 
+    @staticmethod
+    def leave1run_out(classes_runs):
+        """Generator that returns the range of the run left out for test and the rest for fitting
+        
+        Parameters:
+
+        classes_runs: 2-d arraylike
+            Array with first dimension being the number of classes and second being the runs range.
+
+        Yields:
+
+        test_run: numpy.ndarray with shape (1,1)
+            Array with the test run
+        
+        fit_classes_runs: list of lists
+            List with the rest of the runs separated by class
+        """
+    
+        for class_out in range(len(classes_runs)):
+            for run_out in range(len(classes_runs[class_out])):
+                test_run = np.array(classes_runs[class_out][run_out]).reshape(-1,1)
+                train_classes_runs = copy(classes_runs)
+                train_classes_runs[class_out].pop(run_out)
+
+                yield class_out, run_out, test_run, train_classes_runs
+    
     def _compiled(self):
         try:
             self.test_batch
