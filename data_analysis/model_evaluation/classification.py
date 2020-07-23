7af2b3b20d935d3a0ba4a8e7720128648b5b49de
lucasbanunes <lucasbanunes@gmail.com>
2020-07-23 14:04:35 -0300

    Made changes in wta_classf_analysis for cases where the classifier predicts all data correctly.

--- data_analysis/model_evaluation/classification.py
+++ data_analysis/model_evaluation/classification.py
@@ -1,8 +1,9 @@
+import os
 import numpy as np
 import pandas as pd
 from data_analysis.utils import utils
 
-def wta_classf_analysis(output, labels, classes = None):
+def wta_classf_analysis(output, labels, classes = None, filepath = None, verbose=False):
     """Function to analise the classification and neuron output fromn a neural network classifier
     using winner takes all method (wta). It is made considering the alphabetical or numerical order of the labels.
     This means that the first neuron will be the first class in sorted order and so forth. 
@@ -34,8 +35,12 @@ def wta_classf_analysis(output, labels, classes = None):
         num_events = np.sum(unique_counts)
         num_correct_classf = unique_counts[unique_classf == class_][0]
         num_misclassf = np.sum(unique_counts[unique_classf != class_])
-        num_most_misclassf = unique_counts[utils.sec_argmax(unique_counts)]
-        most_misclassf_class = unique_classf[unique_counts == num_most_misclassf][0]
+        if len(unique_counts) > 1:
+            num_most_misclassf = unique_counts[utils.sec_argmax(unique_counts)]
+            most_misclassf_class = unique_classf[unique_counts == num_most_misclassf][0]
+        else:
+            num_most_misclassf = 0
+            most_misclassf_class = None
         class_neuron_output = output[labels == class_].T[class_index]
         avg = np.sum(class_neuron_output)/len(class_neuron_output)
         err = np.sqrt(np.var(class_neuron_output))
@@ -43,5 +48,12 @@ def wta_classf_analysis(output, labels, classes = None):
         min_value = min(class_neuron_output)
         acc = num_correct_classf/num_events
         data.append([num_events, num_correct_classf, avg, err, max_value, min_value, num_misclassf, most_misclassf_class, num_most_misclassf, acc])
-        print(class_, unique_classf, unique_counts)
-    return pd.DataFrame(data, index=classes, columns=['Events', 'Correct', 'Avg', 'Error', 'Max', 'Min', 'Misclassf', 'Most Misclassf', 'Most events', 'Acc'])
+        if verbose:
+            print(class_, unique_classf, unique_counts)
+    frame = pd.DataFrame(data, index=classes, columns=['Events', 'Correct', 'Avg', 'Error', 'Max', 'Min', 'Misclassf', 'Most Misclassf', 'Most events', 'Acc'])
+    if not filepath is None:
+        folder, _ = os.path.split(filepath)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        frame.to_csv(filepath)
+    return frame
